e557548f3f0542e92f01ce9611c804d42f530055
adityaastic <adityaastic@gmail.com>
2025-07-18 21:42:47 +0530

    second commit

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const userRoutes = require('./routes/userRoutes');
+// const paymentRoutes = require('./routes/paymentRoutes');
+
+// dotenv.config();
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// // Routes
+// app.use('/api/users', userRoutes);
+// app.use('/api/payments', paymentRoutes);
+// app.use('/api/payments/webhook', express.raw({ type: 'application/json' }));
+
+// // Connect DB and start server
+// const PORT = process.env.PORT || 5000;
+
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+// .then(() => {
+//     console.log('MongoDB Connected');
+//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// })
+// .catch(err => console.error(err));
+
+
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
@@ -13,7 +44,6 @@ app.use(cors());
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/payments', paymentRoutes);
-app.use('/api/payments/webhook', express.raw({ type: 'application/json' }));
 
 // Connect DB and start server
 const PORT = process.env.PORT || 5000;
