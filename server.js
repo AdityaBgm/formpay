3d02af5d43cc02c5b7559d7a20ae51d586d95111
adityaastic <adityaastic@gmail.com>
2025-07-19 15:04:28 +0530

    -----cors remove11----

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+
+// const paymentRoutes = require('./routes/paymentRoutes');
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.error(err));
+
+// app.use('/api/payment', paymentRoutes);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -6,8 +31,17 @@ require('dotenv').config();
 const paymentRoutes = require('./routes/paymentRoutes');
 
 const app = express();
-app.use(express.json());
+
+// 🔥 Fix CORS completely
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 app.use(cors());
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
